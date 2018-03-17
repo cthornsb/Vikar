e23033a37dfd1ec9e8d44c972dcb6ad4d21576dc
Cory R. Thornsberry <cthornsb@vols.utk.edu>
2018-03-16 21:40:47 -0400

    Added VANDLE det file generator

---
+++ tools/source/detFileMaker.cpp
@@ -0,0 +1,44 @@
+
+
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+#include "vandmc_core.h"
+
+int main(){
+	double radius;
+	double spacing;
+	double startAngle;
+	int Ndet;
+
+	std::cout << " Enter detector radius (m): "; std::cin >> radius;
+	std::cout << " Enter detector spacing (deg): "; std::cin >> spacing;
+	std::cout << " Enter number of detectors: "; std::cin >> Ndet;
+	std::cout << " Enter angle of 1st det (deg): "; std::cin >> startAngle;
+
+	std::string filename;
+	std::cout << " Enter detector filename: "; std::cin >> filename;
+
+	std::ofstream file(filename.c_str());
+	if(!file.good()){
+		std::cout << "  ERROR! Failed to open output file \"" << filename << "\"!\n";
+		return -1;
+	}
+
+	file << "#x\ty\tz\ttheta\tphi\tpsi\ttype\tsybtype\tlength\twidth\tdepth\tmaterial\n";
+
+	double x, y, z, angle;
+	for(int i = 0; i < Ndet; i++){
+		angle = startAngle + spacing*i;
+		Sphere2Cart(radius, angle*deg2rad, 0, x, y, z);
+		file << x << "\t" << y << "\t" << z << "\t" << angle*deg2rad << "\t0\t0\tvandle\tsmall\n";
+	}
+
+	std::cout << "  Done! Wrote file \"" << filename << "\".\n";
+
+	file.close();
+
+	return 0;
+}
